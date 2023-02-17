ce21c1f080f4614421d441e5d2070d3fe6a37d4a
Daemon19 <komanglangendria04@gmail.com>
2023-02-17 22:26:16 +0800

    Create navbar

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const { checkAuthenticated } = require('../auth/check-authenticated');
 const router = express.Router();
 
 router.get('/', checkAuthenticated('/auth/login'), (req, res) => {
-  res.render('index.ejs', { username: req.user.username });
+  res.render('index.ejs', { user: req.user });
 });
 
 module.exports = router;
